a99f568604508b67adfc38bb64be0848af1bde6f
Safian Iddrisu <safiannsterlin@gmail.com>
2024-06-22 12:39:25 +0100

    added new files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+subtract = number1 - number2
+product = number2 * number1
+
+print ("addition of", number1, "and", number2, "is", sum)
+print ("subtraction", number1, "and", number2, "is", subtract)
+print ("product of", number2, "and", number1, "is", product)
+
